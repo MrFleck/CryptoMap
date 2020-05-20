bae1fd9620205da511ce84ab9e43b7ddcc3bb71f
Leonardo <leonardo.lopes555@gmail.com>
2020-05-19 21:54:27 -0300

    criando lógica da página de search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -12,62 +12,26 @@ import { SvgUri } from 'react-native-svg';
 class Search extends Component{
     
     state = {
-        loading: true,
-        prices: [],
+        moedas: [
+            {name: "bitcoin", valor:"1000"},
+            {name: "eth", valor:"1000"},
+            {name: "xpr", valor:"1000"},
+        ]
     };
 
-    componentDidMount(){
-        this.loadCrypto();
-    }
 
-    loadCrypto = async () => {
-        const response = await api.get('/currencies/ticker?key=6ddc22213a53b49c36b5f38de5af8726&ids=BTC,ETH,XPR&interval=1d&convert=BRL');
-        let price = response.data
-        console.log(price)
-        this.setState({ prices: price , loading: false})
-    };
-
-    renderItem = ({ item }) => (
-        <TouchableOpacity onPress={() => { Actions.card(item) }}>
-            <View style={styles.Card}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                    <SvgUri
-                        width={40}
-                        height={40}
-                        uri={item.logo_url}
-                    />
-
-                    <View style = {{textAlign:'left', width:scale(180)}}>
-                        <Text style={styles.CryptoName}>{item.currency}</Text>
-                    </View>
-
-                    <View>
-                        <TouchableOpacity onPress={() => Actions.pop()}>
-                            <Icons name='plus' size={23} style={{ marginRight: scale(1), color: '#fff' }} />
-                        </TouchableOpacity>
-                    </View>
-
+    lista = () => {
+      return this.state.moedas.map(infos => {
+            return (
+                <View>
+                    <Text style ={{color:"#fff"}}>{infos.name}</Text>
                 </View>
-                
-                    <View style ={{top:scale(20)}}>
-                        <Text style={styles.TextPrice}>Preço:</Text>
-                    </View>
-
-                   
-                        <TextMask
-                            style={styles.CryptoDescription}
-                            value={Number(item.price).toFixed(2)}
-                            type={'money'}
-                        />
-
-            </View>
-        </TouchableOpacity>
-    )
-
-
+            );
+        });
+        };
 
     render(){
-       return( 
+       return (
         <View style={styles.Container}>
 
             <View style = {styles.Header}>
@@ -84,32 +48,27 @@ class Search extends Component{
 
             </View>
 
-            <View style={{ marginTop: scale(5), flex: 1,alignItems:'center' }}>
-                <FlatList
-                    data={this.state.prices}
-                    keyExtractor={item => item.currency}
-                    renderItem={this.renderItem}
-                    horizontal={false}
-                    numColumns={1}
-                    style={{ flex: 1, }}
-                 />                   
+            <View>
+                {this.lista()}
             </View>
 
             <View style={{ alignItems: 'center', marginBottom:scale(5) }}>
                 <Image source = {require('../assets/image/logo.png')} />
             </View> 
-                <Text style ={{color:'#fff', fontSize:17, textAlign:'center', marginBottom:scale(10)}}>Sair</Text> 
+            
+            <Text style ={{color:'#fff', fontSize:17, textAlign:'center', marginBottom:scale(10)}}>Sair</Text> 
 
         </View>
-       );
+        );
     }
 } 
+
 
 const styles = StyleSheet.create({
     Container: {
         display: 'flex',
         flex: 1,
-        backgroundColor: '#000000'
+        backgroundColor: '#000'
     },
     Text:{
         color:'#fff',
